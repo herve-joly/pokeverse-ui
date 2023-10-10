c0d12a967cf5ab3aa78a617d1bfa1c8e5dcdea15
= <=>
2023-10-10 12:06:09 +0100

    navbar try

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <div>
+      <!-- Image and text -->
+      <b-navbar variant="faded" type="light">
+        <b-navbar-brand href="#">
+          <img src="https://purepng.com/photo/19587/clipart-pokeball" :alt="'Pokeball'">
+          Pokemon
+        </b-navbar-brand>
+      </b-navbar>
+    </div>
+  </template>
+ 
+  
