fc379c24d6d5d4db57c4c1fdbdc4f9e76fa5f434
Herve Joly <81830836+herve-joly@users.noreply.github.com>
2023-10-10 21:31:03 +0100

    broke everything

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
+import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import './style.scss'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 createApp(App).mount('#app')
+
+import { createApp } from 'vue'
+
